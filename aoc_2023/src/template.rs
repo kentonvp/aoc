6cb88ac693b40bc8bb8450c220c395b6fb695919
kentonvp <kentonvanp@gmail.com>
2023-12-04 21:25:03 -1000

    Template for future

---
+++ aoc_2023/src/template.rs
@@ -0,0 +1,30 @@
+pub fn process1(input: &str) -> u32 {
+    let stime = std::time::Instant::now();
+    println!("Day N - Part 1: {} [{:?}]", sum, stime.elapsed());
+    todo!()
+}
+
+pub fn process2(input: &str) -> u32 {
+    let stime = std::time::Instant::now();
+    println!("Day N - Part 2: {} [{:?}]", sum, stime.elapsed());
+    todo!()
+}
+
+// Test
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+
+    #[test]
+    fn test_process1() {
+        let input = "";
+        assert_eq!(process1(input), 0)
+    }
+
+    #[test]
+    fn test_process2() {
+        let input = "";
+        assert_eq!(process2(input), 0)
+    }
+}
